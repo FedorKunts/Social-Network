afb70c44fd63fb2c1a4b418ba9ae2cdee9ddf8a1
FedorKunts <fedkunts@yandex.ru>
2022-03-23 19:51:56 +0700

    3

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -16,43 +16,46 @@ const MessagesItem = (props) => {
 };
 
 let Messages = (props) => {
+  let dialogsData = [
+    { id: 1, name: "Tom" },
+    { id: 2, name: "Mary" },
+    { id: 3, name: "Jack" },
+    { id: 4, name: "Ammy" },
+    { id: 5, name: "Ron" },
+    { id: 6, name: "Harry" },
+  ];
 
-let dialogsData = [
-  {id: 1, name: 'Tom'},
-  {id: 2, name: 'Mary'},
-  {id: 3, name: 'Jack'},
-  {id: 4, name: 'Ammy'},
-  {id: 5, name: 'Ron'},
-  {id: 6, name: 'Harry'}
-]
+  let dialogsElements = [
+    <DialogsItem id={dialogsData[0].id} name={dialogsData[0].name} />,
+    <DialogsItem id={dialogsData[1].id} name={dialogsData[1].name} />,
+    <DialogsItem id={dialogsData[2].id} name={dialogsData[2].name} />,
+    <DialogsItem id={dialogsData[3].id} name={dialogsData[3].name} />,
+    <DialogsItem id={dialogsData[4].id} name={dialogsData[4].name} />,
+    <DialogsItem id={dialogsData[5].id} name={dialogsData[5].name} />,
+  ];
 
-let messagesData = [
-  {message: 'Hello'},
-  {message: 'Hello'},
-  {message: 'Hello'},
-  {message: 'Hello'},
-  {message: 'Hello'},
-  {message: 'Hello'}
-]
+  let messagesData = [
+    { message: "Hello" },
+    { message: "Hello" },
+    { message: "Hello" },
+    { message: "Hello" },
+    { message: "Hello" },
+    { message: "Hello" },
+  ];
+
+  let messagesElemets = [
+    <MessagesItem message={messagesData[0].message} />,
+    <MessagesItem message={messagesData[1].message} />,
+    <MessagesItem message={messagesData[2].message} />,
+    <MessagesItem message={messagesData[3].message} />,
+    <MessagesItem message={messagesData[4].message} />,
+    <MessagesItem message={messagesData[5].message} />,
+  ];
 
   return (
     <div className={s.dialogsWrapper}>
-      <div className={s.dialogs}>
-        <DialogsItem id={dialogsData[0].id} name={dialogsData[0].name} />
-        <DialogsItem id={dialogsData[1].id} name={dialogsData[1].name} />
-        <DialogsItem id={dialogsData[2].id} name={dialogsData[2].name} />
-        <DialogsItem id={dialogsData[3].id} name={dialogsData[3].name} />
-        <DialogsItem id={dialogsData[4].id} name={dialogsData[4].name} />
-        <DialogsItem id={dialogsData[5].id} name={dialogsData[5].name} />
-      </div>
-      <div className={s.messages}>
-        <MessagesItem message={messagesData[0].message} />
-        <MessagesItem message={messagesData[1].message} />
-        <MessagesItem message={messagesData[2].message} />
-        <MessagesItem message={messagesData[3].message} />
-        <MessagesItem message={messagesData[4].message} />
-        <MessagesItem message={messagesData[5].message} />
-      </div>
+      <div className={s.dialogs}></div>
+      <div className={s.messages}></div>
     </div>
   );
 };
